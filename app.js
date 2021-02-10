12ee6a320f899a6f74a7059d76b7f6e62820b28a
Ander Swift <anderswift@gmail.com>
2021-02-09 20:03:41 -0500

    outline app.js

---
+++ app.js
@@ -0,0 +1,59 @@
+require('dotenv').config();
+
+const { PORT = 3000, DATABASE } = process.env;
+
+const express = require('express');
+const mongoose = require('mongoose');
+const helmet = require('helmet');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+const { errors } = require('celebrate');
+
+// import logger
+// import auth
+
+// import routes
+
+// import error handling
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 200,
+});
+
+const app = express();
+
+app.use(cors());
+app.options('*', cors());
+
+app.use(limiter);
+
+mongoose.connect(DATABASE, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+app.use(helmet());
+app.use(express.json());
+
+// for crash testing, remove after review
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Server will crash now');
+  }, 0);
+});
+
+// account routes
+
+// authorization
+
+// protected routes
+
+// wildcard to catch everything else as 404
+
+// celebrate error handler
+app.use(errors());
+
+app.listen(PORT);
